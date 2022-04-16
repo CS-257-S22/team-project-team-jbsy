bb243fabaa223850d1f4916abd2146dd171dd301
Bennet Tefu <tefub@carleton.edu>
2022-04-16 14:56:48 -0500

    created methods: top10VisaApprovalRatesByYear(), approvalRates(), approvalRatesByCompany(company), getCompaniesByMinContinuingApproval(continuingApproval)

--- project.py
+++ project.py
@@ -1,9 +1,11 @@
-filepath = 'h1b_datahubexport-2018.csv';
+filepath = 'dummyData.csv';
 database = open(filepath,"r");
+database.readline();
+approvalPercentages = [];
 
 def getCompaniesByState(state):
    substring = "\"" + state + "\"";
-   for i in range(1000):
+   for i in range(60):
        lineData = database.readline()
        databaseInfo = lineData.split(",");
        if(databaseInfo[8]==substring):
@@ -11,7 +13,7 @@ def getCompaniesByState(state):
 
 def getStatByCompanies(company):
      substring = "\"" + company + "\"";
-     for i in range(1000):
+     for i in range(60):
         lineData = database.readline()
         databaseInfo = lineData.split(",");
         if(databaseInfo[1]==substring):
@@ -28,17 +30,49 @@ def getStatByCompanies(company):
             print("ZIP => " + databaseInfo[10]);
 
 def getCompaniesByMinInitApproval(initApproval):
-    substring = initApproval;
+    #substring = initApproval;
     database.readline();
-    for i in range(10000):
+    for i in range(60):
         lineData = database.readline()
         databaseInfo = lineData.split(",");
         if(int(databaseInfo[2].replace("\"",""))>=initApproval):
             print("Employer => " + databaseInfo[1]);
 
+def getCompaniesByMinContinuingApproval(continuingApproval):
+    #substring = continuingApproval;
+    database.readline();
+    for i in range(60):
+        lineData = database.readline();
+        databaseInfo = lineData.split(",");
+        if(int(databaseInfo[4].replace("\"",""))>=continuingApproval):
+            print("Employer => " + databaseInfo[1]);
 
-         
+def approvalRatesByCompany(company):
+    approvalRates();
+    for i in range(60):
+        if (approvalPercentages[i][1]==company):
+            print(str(approvalPercentages[i][0]));
+            
+
+def approvalRates():
+    for i in range(60):
+        companyInfo = [];
+        lineData = database.readline();
+        databaseInfo = lineData.split(",");
+        companyInfo.append(100 * (int(databaseInfo[2].replace("\"","")) + int(databaseInfo[4].replace("\"","")))/2);
+        companyInfo.append(databaseInfo[1].replace("\"",""));
+        approvalPercentages.append(companyInfo);
+
+def top10VisaApprovalRatesByYear():
+    approvalRates();
+    approvalPercentages.sort(reverse=True);
+    for i in range(10):
+     print(approvalPercentages[i][1] + " => " + str(approvalPercentages[i][0]));
 
 #getStatByCompanies("CVE TECHNOLOGY GROUP INC");
-# print(getCompaniesByState("SD"));
-#getCompaniesByMinInitApproval(2);
+#print(getCompaniesByState("SD"));
+#print(getCompaniesByMinInitApproval(1));
+#print(getCompaniesByMinContinuingApproval(1));
+#top10VisaApprovalRatesByYear();
+approvalRatesByCompany("SAN JOSE STATE UNIVERSISTY");
+#approvalPercentage()
