d87f22bab4e4d5de856e1f827232619c381115fc
Joshua Song <joshuarule2010@gmail.com>
2022-04-16 14:26:54 -0500

    created general frame for read command line

--- project.py
+++ project.py
@@ -1,3 +1,6 @@
+import sys
+from helper import readFile
+
 filepath = 'h1b_datahubexport-2018.csv';
 database = open(filepath,"r");
 
@@ -27,7 +30,7 @@ def getStatByCompanies(company):
             print("City => " + databaseInfo[9]);
             print("ZIP => " + databaseInfo[10]);
 
-def getCompaniesByMinInitApproval(initApproval):
+def getCompaniesByMinInitApproval(initApproval, target):
     substring = initApproval;
     database.readline();
     for i in range(10000):
@@ -36,8 +39,41 @@ def getCompaniesByMinInitApproval(initApproval):
         if(int(databaseInfo[2].replace("\"",""))>=initApproval):
             print("Employer => " + databaseInfo[1]);
 
+def initiateCommand(command, target):
+    if "company" in command:
+        #call getStatByCompanies
+    elif "state" in command:
+        # call getCompaniesByState
+    elif "initApproval" in command:
+        # call minimum and maximum then print
+    elif "ContinuingApprovals" in command:
+        # call minimum and maximum then print
+    else:
+        print("Please input a command that is valid\n")
 
-         
+def readCommandLine():
+    # Read the commandline as arg
+    arg = sys.argv
+
+    # Todo: Verify if arg data is correct
+
+    fileData = readFile(arg[1])
+    visaData = fileData[0]
+    mostRecentYear = fileData[1]
+    
+    command = arg[2]
+    target = arg[-1]
+
+    # Todo: Verify if target relevant (company name exists, or column name exists, etc)
+
+    initiateCommand(command, target)
+    
+
+
+# project.py dummyData.csv --company "Google"
+
+# Commandline example
+# python3 project.py dummyData.csv --company "Google"
 
 #getStatByCompanies("CVE TECHNOLOGY GROUP INC");
 # print(getCompaniesByState("SD"));
