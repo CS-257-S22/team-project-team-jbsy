7b47418a43364828c7e9569e66b377bdec823764
yobo1889 <yobo1889@protonmail.com>
2022-04-17 22:20:43 -0500

    service

--- service.py
+++ service.py
@@ -1,7 +1,9 @@
-from helper import printCompaniesInState, printCompany, printMinInitApproval
+from helper import printCompaniesInState, printCompany, printMinInitApproval, printUsage
 
 # method to get the names of companies within the input state
+
 def getCompaniesByState(arguments):
+    
    state = arguments["target"] 
    visaData = arguments["visaData"]
    mostRecentYear = arguments["mostRecentYear"]
@@ -33,7 +35,6 @@ def getCompaniesByMinInitApproval(arguments):
         # raise ValueError
         return []
         
-
     initApproval  = arguments["target"]
     visaData = arguments["visaData"]
     mostRecentYear = arguments["mostRecentYear"]
@@ -45,27 +46,6 @@ def getCompaniesByMinInitApproval(arguments):
             companyList.append({"companyName": j, "data": visaData[j][mostRecentYear]})        
     return companyList;      
 
-def approvalRatesByCompany(company):
-    approvalRates();
-    for i in range(60):
-        if (approvalPercentages[i][1]==company):
-            print(str(approvalPercentages[i][0]));
-
-def approvalRates():
-    for i in range(60):
-        companyInfo = [];
-        lineData = database.readline();
-        databaseInfo = lineData.split(",");
-        companyInfo.append(100 * (int(databaseInfo[2].replace("\"","")) + int(databaseInfo[4].replace("\"","")))/2);
-        companyInfo.append(databaseInfo[1].replace("\"",""));
-        approvalPercentages.append(companyInfo);
-
-def top10VisaApprovalRatesByYear():
-    approvalRates();
-    approvalPercentages.sort(reverse=True);
-    for i in range(10):
-     print(approvalPercentages[i][1] + " => " + str(approvalPercentages[i][0]));
-    
 def initiateCommand(argument):
     command = argument["command"]
     visaData = argument["visaData"]
@@ -81,31 +61,12 @@ def initiateCommand(argument):
     elif "state" in command:
         companyList = getCompaniesByState({"visaData": visaData, "target": target, "mostRecentYear": mostRecentYear})
         printCompaniesInState(companyList, target)
-
-    # call minimum and maximum then print
-    elif "initApproval" in command:
-        result = getCompaniesByColumn({"visaData": visaData, "columnName": "Initial Approvals", "mostRecentYear": mostRecentYear})
-
-        minCompanies = result[0]
-        maxCompanies = result[1]
-
-        printMinAndMaxData({"maxList": maxCompanies, "minList": minCompanies, "mostRecentYear": mostRecentYear, "columnName": "Initial Approvals"})
     
     elif "minInitApproval" in command:
         result = getCompaniesByMinInitApproval({"visaData": visaData, "target": target, "mostRecentYear": mostRecentYear})
-
-        # companyList = result[0]
-
         printMinInitApproval({"companiesList":result, "target": target, "mostRecentYear": mostRecentYear})
-    
-    # call minimum and maximum then print
-    elif "continuingApproval" in command:
-        result = getCompaniesByColumn({"visaData": visaData, "columnName": "Continuing Approvals", "mostRecentYear": mostRecentYear})
-
-        minCompanies = result[0]
-        maxCompanies = result[1]
-
-        printMinAndMaxData({"maxList": maxCompanies, "minList": minCompanies, "mostRecentYear": mostRecentYear, "columnName": "Continuing Approvals"})
+    elif "usage" in command:
+        printUsage();
     else:
         print("Please input a command that is valid\n")
 
